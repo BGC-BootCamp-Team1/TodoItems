dc973e5c3411f375f98e6da7e2a3eafe1f681f6c
tangqi <qtang5@slb.com>
2024-10-31 17:41:41 +0800

    Feat: add DueDate in TodoItem, and update UT

--- TodoItems.Core/TodoItem.cs
+++ TodoItems.Core/TodoItem.cs
@@ -4,16 +4,18 @@ namespace TodoItems.Core;
 
 public class TodoItem
 {
+    public DateTimeOffset DueDate { get; set; }
     public string Id { get; set; }
     public string Description { get; set; }
     public DateTimeOffset CreateTime { get; set; }
     public List<Modification> ModificationHistory { get; set; }
-    public TodoItem(string description)
+    public TodoItem(string description, DateTimeOffset dueDate)
     {
         Id = Guid.NewGuid().ToString();
         Description = description;
         CreateTime = DateTimeOffset.Now;
         ModificationHistory = new List<Modification>();
+        DueDate = dueDate;
     }
     public void ModifyItem(string modifiedDescription)
     {
