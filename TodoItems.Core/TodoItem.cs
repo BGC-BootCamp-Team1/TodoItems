eceb856924ccabe4681be9263d8d60ae03fb4f0d
tangqi <qtang5@slb.com>
2024-10-31 17:23:43 +0800

    Feat: implement TodoItem class and Modification class

--- TodoItems.Core/TodoItem.cs
+++ TodoItems.Core/TodoItem.cs
@@ -1,9 +1,39 @@
-﻿namespace TodoItems.Core;
+﻿using System.Security.AccessControl;
+
+namespace TodoItems.Core;
 
 public class TodoItem
 {
-    public string GetId()
+    public string Id { get; set; }
+    public string Description { get; set; }
+    public DateTimeOffset CreateTime { get; set; }
+    public List<Modification> ModificationHistory { get; set; }
+    public TodoItem(string description)
     {
-        return "1";
+        Id = Guid.NewGuid().ToString();
+        Description = description;
+        CreateTime = DateTimeOffset.Now;
+        ModificationHistory = new List<Modification>();
+    }
+    public void ModifyItem(string modifiedDescription)
+    {
+        if (ModificationHistory.Count(m => m.TimesStamp.Date == DateTime.Today) < 3)
+        {
+            ModificationHistory.Add(new Modification());
+            Description = modifiedDescription;
+        }
+        else
+            throw new Exception("Modification Limit.");
+
     }
 }
+
+public class Modification
+{
+    public DateTimeOffset TimesStamp { get; set; }
+    public Modification()
+    {
+        TimesStamp = DateTimeOffset.Now;
+    }
+}
+
