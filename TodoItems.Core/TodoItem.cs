3d597119479c8294d61e3039a981a56b31cf7835
tangqi <qtang5@slb.com>
2024-10-31 17:32:42 +0800

    Feat: implement User class

--- TodoItems.Core/TodoItem.cs
+++ TodoItems.Core/TodoItem.cs
@@ -37,3 +37,12 @@ public class Modification
     }
 }
 
+public class User
+{
+    public List<TodoItem> TodoItems { get; set; }
+    public User()
+    {
+        TodoItems = new List<TodoItem>();
+    }
+}
+
