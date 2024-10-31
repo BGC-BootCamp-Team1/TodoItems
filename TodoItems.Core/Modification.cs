1260c9ffc145b474d74ecd3878763fea12587820
htian15 <htian15@slb.com>
2024-10-31 18:02:11 +0800

    feat: extract Modification class

---
+++ TodoItems.Core/Modification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core
+{
+    public class Modification
+    {
+        public DateTime ModificationTimestamp { get; private set; }
+        public Modification(DateTime date)
+        {
+            this.ModificationTimestamp = date;
+        }
+    }
+}
--- TodoItems.Core/TodoItem.cs
+++ TodoItems.Core/TodoItem.cs
@@ -1,9 +1,48 @@
-﻿namespace TodoItems.Core;
+﻿
+namespace TodoItems.Core;
 
 public class TodoItem
 {
-    public string GetId()
+    private string id;
+    public string Description { get; set; }
+    public List<Modification> ModificationTimestamps;
+
+
+
+    public TodoItem(string id, string description, List<Modification> modificationTimestamps)
     {
-        return "1";
+        this.id = id;
+        this.Description = description;
+        this.ModificationTimestamps = modificationTimestamps;
+    }
+
+    public class TodoItemTimestamps
+    {
+        
+    }
+
+    public void AddTimestamp()
+    {
+        DateTime now = DateTime.Now;
+        DateTime today = now.Date;
+        const int MaximumNumberInOneDay = 3;
+
+        
+        int todayCount = ModificationTimestamps.Count(ts => ts.ModificationTimestamp.Date == today);
+
+       
+        if (todayCount >= MaximumNumberInOneDay)
+        {
+            return;
+        }
+
+    }
+
+
+    public void ModifyDescription(string newDescription)
+    {  
+        const string ErrorMessage = "You have reached the maximum number of modifications for today. Please try agian tomorrow.";
+        throw new ArgumentException(ErrorMessage);
+        
     }
 }
