fdcc25ab0176f19c2039e6a17f361e5a87ddbb1e
htian15 <htian15@slb.com>
2024-11-01 00:07:16 +0800

    feat: add TodoItemService

---
+++ TodoItems.Core/TodoItemService.cs
@@ -0,0 +1,36 @@
+﻿
+namespace TodoItems.Core;
+
+public class TodoItemService
+{
+    private readonly ITodoRepository _repository;
+    public List<TodoItem> TodoItems { get; private set; }
+
+    public TodoItemService(ITodoRepository repository)
+    {
+        _repository = repository;
+        TodoItems = new List<TodoItem>();
+    }
+
+    public TodoItem CreateTodoItem(string description, DateTime dueDate)
+    {
+        if(dueDate < DateTime.Now)
+        {
+            throw new ArgumentException("Due date cannot be in the past.");
+        }
+        var count = _repository.CountTodoItemsOnDueDate(dueDate);
+        if (count < 8) {
+            var todoItem = new TodoItem(description, dueDate);
+
+            TodoItems.Add(todoItem);
+            return todoItem;
+        }
+        else
+        {
+            throw new ArgumentException("You have reached the maximum number of todo items for this due date.");
+        }
+        
+    }
+
+    
+}
