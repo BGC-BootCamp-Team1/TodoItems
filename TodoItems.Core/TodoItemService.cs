760fdcf1a9674558a1837b06081e1981f02adc31
Ting Li <TLi26@slb.com>
2024-10-31 19:07:37 +0800

    test: add test for TodoItemService

---
+++ TodoItems.Core/TodoItemService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core
+{
+    public class TodoItemService
+    {
+        private readonly ITodosRepository _todosRepository;
+        private readonly int _maxItemPerDueDate = 8;
+
+        public TodoItemService(ITodosRepository repository)
+        {
+            _todosRepository = repository;
+        }
+        public TodoItem createItem(string description, DateTime dueDate) {
+            if (_todosRepository.FindAllTodoItemsHaveTheSameDueDate(dueDate).Count < _maxItemPerDueDate)
+            {
+                TodoItem item = new TodoItem
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Description = description,
+                    Modifications = [],
+                    DueDate = dueDate
+                };
+                return item;
+            } 
+            else 
+            {
+                throw new Exception();
+            }
+            
+        }
+
+
+    }
+}
---
+++ TodoItems.Test/TodoItemServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using TodoItems.Core;
+using Moq;
+
+namespace TodoItems.Test
+{
+    public class TodoItemServiceTest
+    {
+        [Fact]
+        public void should_throw_exception_when_create_nineth_item()
+        {
+            DateTime dueDate = new DateTime(2024,10,31);
+            List<TodoItem> mockTodoItems = new List<TodoItem>();
+            for (int i = 0; i < 8; i++)
+            {
+                TodoItem item = new TodoItem
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Description = "Task " + (i + 1),
+                    Modifications = [],
+                    DueDate = dueDate
+                };
+                mockTodoItems.Add(item);
+            }
+
+            var mockRepository = new Mock<ITodosRepository>();
+            mockRepository.Setup(repo => repo.FindAllTodoItemsHaveTheSameDueDate(dueDate))
+                .Returns(mockTodoItems);
+
+            var service = new TodoItemService(mockRepository.Object);
+            
+            Assert.Throws<Exception>(() => service.createItem("test", dueDate));
+        }
+
+        [Fact]
+        public void should_create_when_create_second_item()
+        {
+            DateTime dueDate = new DateTime(2024, 10, 31);
+            List<TodoItem> mockTodoItems = new List<TodoItem>();
+            for (int i = 0; i < 1; i++)
+            {
+                TodoItem item = new TodoItem
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Description = "Task " + (i + 1),
+                    Modifications = [],
+                    DueDate = dueDate
+                };
+                mockTodoItems.Add(item);
+            }
+
+            var mockRepository = new Mock<ITodosRepository>();
+            mockRepository.Setup(repo => repo.FindAllTodoItemsHaveTheSameDueDate(dueDate))
+                .Returns(mockTodoItems);
+
+            var service = new TodoItemService(mockRepository.Object);
+            var actualTodoItem = service.createItem("test", dueDate);
+            var expectedTodoItem = new TodoItem
+            {
+                Id = Guid.NewGuid().ToString(),
+                Description = "test",
+                Modifications = [],
+                DueDate = dueDate
+            };
+
+            Assert.Equal(actualTodoItem.Description, expectedTodoItem.Description);
+            Assert.Equal(actualTodoItem.DueDate, expectedTodoItem.DueDate);
+        }
+    }
+}
