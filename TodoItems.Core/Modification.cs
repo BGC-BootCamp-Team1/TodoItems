cd7130f32ad851e720c163d9c74d379ace957c32
Sheng Liu <SLiu40@slb.com>
2024-10-31 17:41:26 +0800

    FEAT.add modify func and ut

---
+++ TodoItems.Core/Modification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core
+{
+    public class Modification
+    {
+        //public string NewDescription { get; set; }
+        //public string OldDescription { get; set; }
+        public DateTime TimeStamp {  get; set; }
+
+        public Modification()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
--- TodoItems.Test/TodoItemTest.cs
+++ TodoItems.Test/TodoItemTest.cs
@@ -5,9 +5,59 @@ namespace TodoItems.Test;
 public class TodoItemTest
 {
     [Fact]
-    public void should_return_2_when_add_1_1()
+    public void should_return_todoItem_when_create()
     {
-        var todoItem = new TodoItem();
-        Assert.Equal("1", todoItem.GetId());
+        TodoItem item = new TodoItem();
+        Assert.NotNull(item._id);
+        Assert.NotNull(item.Description);
+        Assert.NotNull(item.ModificationList);
     }
+    [Fact]
+    public void should_contain_1_modification_when_modify()
+    {
+        TodoItem item = new TodoItem();
+        item.Modify("newDes");
+        Assert.Single(item.ModificationList);
+    }
+
+    [Fact]
+    public void should_throw_exception_when_more_than_3_today()
+    {
+        TodoItem item = new TodoItem();
+        item.Modify("1");
+        item.Modify("2");
+        item.Modify("3");
+
+        Assert.Throws<NotificationException>(() =>
+        {
+            item.Modify("4");
+        });
+
+    }
+
+    [Fact]
+    public void should_not_throw_exception_when_less_than_3_today()
+    {
+        TodoItem item = new TodoItem();
+        item.Modify("1");
+        item.Modify("2");
+        item.Modify("3");
+
+        item.ModificationList[0].TimeStamp = item.ModificationList[0].TimeStamp.AddDays(-1);
+        item.ModificationList[1].TimeStamp = item.ModificationList[1].TimeStamp.AddDays(-2);
+        var exception = Record.Exception(() =>
+        {
+            item.Modify("4");
+        });
+
+        // Assert
+        Assert.Null(exception);
+
+
+    }
+
+
+
+
+
 }
