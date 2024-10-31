a66b360b41cfddf18ef4f9ad857c71b1a0338c3c
tangqi <qtang5@slb.com>
2024-10-31 17:22:41 +0800

    Test: add TodoItem UT

---
+++ TodoItems.CoreTests/TodoItemTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoItems.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core.Tests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        [TestMethod]
+        public void ModifyItem_ShouldUpdateDescription_WhenUnderLimit()
+        {
+            // Arrange
+            var todoItem = new TodoItem("Initial Description");
+
+            // Act
+            todoItem.ModifyItem("Modified Description");
+
+            // Assert
+            Assert.AreEqual("Modified Description", todoItem.Description);
+            Assert.AreEqual(1, todoItem.ModificationHistory.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "Modification Limit.")]
+        public void ModifyItem_ShouldThrowException_WhenOverLimit()
+        {
+            // Arrange
+            var todoItem = new TodoItem("Initial Description");
+
+            // Add three modifications for today
+            todoItem.ModifyItem("Modified Description 1");
+            todoItem.ModifyItem("Modified Description 2");
+            todoItem.ModifyItem("Modified Description 3");
+
+            // Act
+            todoItem.ModifyItem("Modified Description 4");
+
+            // Assert is handled by ExpectedException
+        }
+
+        [TestMethod]
+        public void ModifyItem_ShouldAllowModification_WhenNewDay()
+        {
+            // Arrange
+            var todoItem = new TodoItem("Initial Description");
+
+            // Add three modifications for today
+            todoItem.ModifyItem("Modified Description 1");
+            todoItem.ModifyItem("Modified Description 2");
+            todoItem.ModifyItem("Modified Description 3");
+
+            // Simulate a new day
+            foreach (var mod in todoItem.ModificationHistory)
+            {
+                mod.TimesStamp = mod.TimesStamp.AddDays(-1);
+            }
+
+            // Act
+            todoItem.ModifyItem("Modified Description New Day");
+
+            // Assert
+            Assert.AreEqual("Modified Description New Day", todoItem.Description);
+            Assert.AreEqual(4, todoItem.ModificationHistory.Count);
+        }
+    }
+}
--- TodoItems.Test/TodoItemTest.cs
+++ TodoItems.Test/TodoItemTest.cs
@@ -7,7 +7,7 @@ public class TodoItemTest
     [Fact]
     public void should_return_2_when_add_1_1()
     {
-        var todoItem = new TodoItem();
+        
         Assert.Equal("1", todoItem.GetId());
     }
 }
