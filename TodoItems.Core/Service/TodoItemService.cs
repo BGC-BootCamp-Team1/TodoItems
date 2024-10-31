07a49adceab934d7e6396d4ac624cf5c5bee6897
Sheng Liu <SLiu40@slb.com>
2024-10-31 19:25:55 +0800

    TEST.add test on Create method

--- TodoItems.Core/Service/TodoItemService.cs
+++ TodoItems.Core/Service/TodoItemService.cs
@@ -1,25 +1,35 @@
-﻿using TodoItems.Core.Model;
+﻿using TodoItems.Core.BizException;
+using TodoItems.Core.Model;
 using TodoItems.Core.Repository;
 
 namespace TodoItems.Core.Service
 {
     public class TodoItemService
     {
-        private List<TodoItem> todoItems;
+        public List<TodoItem> itemList;
         private ITodosRepository _repository;
 
         public TodoItemService(ITodosRepository repository)
         {
-            todoItems = new List<TodoItem>();
+            itemList = new List<TodoItem>();
             _repository = repository;
         }
 
         public TodoItem Create(string description, DateOnly dueDay, string userId)
         {
-            return null;
-            
-        }
+            int itemCount = itemList
+                .Count(item => item.UserId == userId && item.DueDay == dueDay);
 
+            if (itemCount >= 8) 
+            {
+                throw new NotificationException($"too many items in same day for {userId}");
+            }
+
+            TodoItem todoItem = new TodoItem(description, dueDay, userId); ;
+            itemList.Add(todoItem);
+            return todoItem;
+
+        }
 
     }
 }
---
+++ TodoItems.Test/TodoItemServiceTest.cs
@@ -0,0 +1,64 @@
+﻿using Moq;
+using TodoItems.Core.BizException;
+using TodoItems.Core.Model;
+using TodoItems.Core.Repository;
+using TodoItems.Core.Service;
+
+namespace TodoItems.Test;
+public class TodoItemServiceTest
+{
+    private readonly TodoItemService _service;
+    private readonly Mock<ITodosRepository> _mockedTodosRepository;
+
+    public TodoItemServiceTest() {
+        _mockedTodosRepository= new Mock<ITodosRepository>();
+        _service = new TodoItemService(_mockedTodosRepository.Object);
+    }
+
+    [Fact]
+    public void should_contains_todoItem_when_create()
+    {
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now), "user1");
+        Assert.Single(_service.itemList);
+    }
+
+    [Fact]
+    public void Can_create_items_10_dueDay()
+    {
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(2)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(3)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(4)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(5)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(6)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(7)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(8)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(9)), "user1");
+
+        Assert.Equal(10,_service.itemList.Count);
+    }
+
+    [Fact]
+    public void Cannot_create_items_same_dueDay()
+    {
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+        _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+
+        Assert.Throws<NotificationException>(() =>
+        {
+            _service.Create("Des", DateOnly.FromDateTime(DateTime.Now.AddDays(1)), "user1");
+        });
+
+    }
+
+
+
+}
+
