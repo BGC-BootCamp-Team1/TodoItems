5f4380f3c32ec397625024fcbf89a58534051fb6
Ting Li <TLi26@slb.com>
2024-10-31 17:41:06 +0800

    feat: add DueDate for TodoItem

--- TodoItems.Core/TodoItem.cs
+++ TodoItems.Core/TodoItem.cs
@@ -7,12 +7,14 @@ public class TodoItem
     public string Id { get; set; } 
     public string Description { get; set; }
     public IList<Modification> Modifications { get; set; }
+    public DateTime DueDate { get; set; }
 
     private readonly int _maxNumberOfModification = 3;
 
     public void Modify(string description)
     {
-        int count = Modifications.Count(modification => modification.TimeStamp.Date == DateTime.Today);
+        DateTime today = DateTime.Today;
+        int count = Modifications.Count(modification => modification.TimeStamp.Date == today);
         if (count < _maxNumberOfModification)
         {
             Description = description;
