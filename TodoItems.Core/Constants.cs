8c1b7d75c4a2ec7cda1be9c1d1be544f296696c2
htian15 <htian15@slb.com>
2024-11-01 19:56:09 +0800

    refactor:misterious name and magic number

---
+++ TodoItems.Core/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoItems.Core;
+
+public static class Constants
+{
+    public const int MaxTodoItemsPerDay = 8;
+    public const int SystemSetDuedateScope = 5;
+}
---
+++ TodoItems.Core/TodoItemObject.cs
@@ -0,0 +1,40 @@
+﻿
+namespace TodoItems.Core;
+
+public class TodoItemObject
+{
+    public string Id { get; init; }
+    public string Description { get; private set; }
+    public List<Modification> ModificationTimestamps { get; set; }
+    public DateTime? DueDate { get; init; }
+
+
+    public TodoItemObject(string description, DateTime dueDate)
+    {
+        Id = Guid.NewGuid().ToString();
+        Description = description;
+        ModificationTimestamps = new List<Modification>();
+        DueDate = dueDate;
+    }
+
+
+
+    public void ModifyDescription(string newDescription)
+    {
+        const int MaximumNumberInOneDay = 3;
+        const string ErrorMessage = "You have reached the maximum number of modifications for today. Please try agian tomorrow.";
+
+        DateTime today = DateTime.Today;
+        int todayCount = this.ModificationTimestamps.Count(ts => ts.ModificationTimestamp.Date == today);
+
+        if (todayCount >= MaximumNumberInOneDay)
+        {
+            throw new ArgumentException(ErrorMessage);
+        }
+        else
+        {
+            this.Description = newDescription;
+            this.ModificationTimestamps.Add(new Modification(DateTime.Now));
+        }
+    }
+}
