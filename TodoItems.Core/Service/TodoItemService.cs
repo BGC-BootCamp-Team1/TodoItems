6b146b3856ff8cdeec92e06a6cad1c369e3119ba
tangqi <qtang5@slb.com>
2024-11-04 20:20:26 +0800

    refactor: remove todoitem list in service

--- TodoItems.Core/Service/TodoItemService.cs
+++ TodoItems.Core/Service/TodoItemService.cs
@@ -5,12 +5,10 @@ namespace TodoItems.Core;
 
 public partial class TodoItemService
 {
-    public List<TodoItem> TodoItems { get; set; }
     private ITodosRepository _todosRepository;
     private Dictionary<CreateOptionEnum,ICreateTodoStrategy> _strategies;
     public TodoItemService(ITodosRepository todosRepository)
     {
-        TodoItems = new List<TodoItem>();
         _todosRepository = todosRepository;
         _strategies = new Dictionary<CreateOptionEnum, ICreateTodoStrategy>
         {
@@ -27,10 +25,10 @@ public partial class TodoItemService
         var strategy = _strategies[createOption];
         var newTodoItem = strategy.Create(description, dueDate, _todosRepository);
 
-        TodoItems.Add(newTodoItem);
-        await _todosRepository.SaveAsync(newTodoItem);
+/*        TodoItems.Add(newTodoItem);
+*/        await _todosRepository.SaveAsync(newTodoItem);
         return newTodoItem;
-
     }
+    public async Modify()
 }
 
